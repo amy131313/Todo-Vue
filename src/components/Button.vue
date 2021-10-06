f46a98e98fdfa15e689580f2482adfc1f5a5270a
Ana-Maria <ana.maria.bulat@accenture.com>
2021-10-06 14:20:40 +0200

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,18 @@
+<template>
+    <button @click="this.$emit('show-addTask')" :style="{ background: color }" class="btn">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: 'Button',
+    props: {
+        text: String, 
+        color: String
+    },
+    methods: {
+        onClick() {
+            this.$emit('show-addTask')
+        }
+    }
+}
+</script>
---
+++ src/components/Task.vue
@@ -0,0 +1,40 @@
+<template>
+    <div @dblclick="$emit('toggle-reminder', task.id)" :class="[task.reminder ? 'reminder' : '', 'task']">
+        <h4>{{ task.text }} 
+            <i @click="this.$emit('delete-task', id)" class="fas fa-times"/>
+        </h4>
+        <p>{{ task.day }}</p>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Task',
+    props: {
+        task: Object
+    },
+}
+</script>
+
+<style scope>
+.fas {
+    color: red;
+}
+.task {
+    background: #f4f4f4;
+    margin: 5px;
+    padding: 20x 20px;
+    cursor: pointer;
+}
+
+.task.reminder {
+    border-left: 5px solid green;
+}
+
+.task h3 {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+</style>
